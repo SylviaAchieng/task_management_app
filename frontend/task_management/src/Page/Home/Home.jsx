843704daa70458f32bb7f40054eb3099ff6877f7
SylviaAchieng <achiengsylvia157@gmail.com>
2024-04-08 09:35:16 +0300

    update

---
+++ frontend/task_management/src/Page/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from '../Sidebar/Sidebar'
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <Sidebar/>
+      </div>
+      <div className='right-side-part'></div>
+    </div>
+  )
+}
