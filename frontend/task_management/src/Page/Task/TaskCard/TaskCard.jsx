802a926f50441e1e8d2e118f8e93b6c7a5fc654a
SylviaAchieng <achiengsylvia157@gmail.com>
2024-04-13 15:29:15 +0300

    build

--- frontend/task_management/src/Page/Task/TaskCard/TaskCard.jsx
+++ frontend/task_management/src/Page/Task/TaskCard/TaskCard.jsx
@@ -1,12 +1,44 @@
 import React from 'react'
+import {IconButton} from '@mui/material'
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
+
+const role ="ROLE_ADMIN"
 export default function TaskCard() {
+
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const openMenu = Boolean(anchorEl);
+  const handleMenuClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleOpenUserList=()=>{
+
+  }
+
+  const handleOpenSubmissionList=()=>{
+    
+  }
+  const handleOpenUpdateTaskModel=()=>{
+    
+  }
+  const handleDeleteTask=()=>{
+    
+  }
+
+
   return (
     <div>
       <div className='card lg:flex justify-between'>
         <div className='lg:flex gap-5 items-center space-y-2 w-[90%] lg:w-[70%]'>
             <div className=''>
-                <img className='lg:w-[7rem] lg:h-[] object-cover' src='https://images.unsplash.com/photo-1503376780353-7e6692767b70?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt=''/>
+                <img className='lg:w-[7rem] lg:h-[7rem] object-cover' src='https://images.unsplash.com/photo-1503376780353-7e6692767b70?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt=''/>
             </div>
 
             <div className='space-y-5'>
@@ -25,6 +57,37 @@ export default function TaskCard() {
                 </div>
 
             </div>
+        </div>
+        <IconButton  id="basic-button"
+        aria-controls={openMenu ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={openMenu ? 'true' : undefined}
+        onClick={handleMenuClick}>
+          <MoreVertIcon/>
+        </IconButton>
+        <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={openMenu}
+        onClose={handleMenuClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        
+        {
+          role==="ROLE_ADMIN"?<>
+          <MenuItem onClick={handleOpenUserList}>Assigned User</MenuItem>
+          <MenuItem onClick={handleOpenSubmissionList}>See Submissions</MenuItem>
+          <MenuItem onClick={handleOpenUpdateTaskModel}>Edit</MenuItem>
+          <MenuItem onClick={handleDeleteTask}>Delete</MenuItem>
+          </>:<>
+          </>
+        }
+     </Menu>
+
+        <div>
+
         </div>
 
       </div>
